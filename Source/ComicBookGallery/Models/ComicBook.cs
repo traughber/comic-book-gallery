b41351b505c3dde3c5a33e6094c035d0fc1640c6
Kris Traughber <traughberk@gmail.com>
2017-03-28 22:19:41 -0700

    Added Comic Book and Artist models.

---
+++ Source/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public bool Favorite { get; set; }
+        public Artist[] Artist { get; set; }
+
+    }
+}
